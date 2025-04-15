a6fe891457e65328c395286566805f1784904c98
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-15 22:41:40 +0900

    feat: 상수 추가

---
+++ src/advanced/constants.ts
@@ -0,0 +1,19 @@
+import { IProduct } from "./types";
+
+const PRODUCTS: IProduct[] = [
+  { id: "p1", name: "상품1", price: 10000, quantity: 50 },
+  { id: "p2", name: "상품2", price: 20000, quantity: 30 },
+  { id: "p3", name: "상품3", price: 30000, quantity: 20 },
+  { id: "p4", name: "상품4", price: 15000, quantity: 0 },
+  { id: "p5", name: "상품5", price: 25000, quantity: 10 },
+];
+
+const QUANTITY_DISCOUNT_RATES = {
+  p1: 0.1,
+  p2: 0.15,
+  p3: 0.2,
+  p4: 0.05,
+  p5: 0.25,
+};
+
+export { PRODUCTS, QUANTITY_DISCOUNT_RATES };
