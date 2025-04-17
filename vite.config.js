ce384c8f3041cf09739edb6734f27447fd97a845
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-18 01:16:23 +0900

    feat: 배포 관련 코드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        basic: resolve(__dirname, 'index.basic.html'),
+        advanced: resolve(__dirname, 'index.advanced.html'),
+      },
+    },
+  },
+});
