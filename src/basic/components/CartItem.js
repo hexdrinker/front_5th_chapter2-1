495e56af88dac68df113275ba43a5310f5db5a16
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-17 21:09:28 +0900

    refactor: CartItem 컴포넌트 리팩토링; 불필요한 값 제거, 분기처리 개선

--- src/basic/components/CartItem.js
+++ src/basic/components/CartItem.js
@@ -1,24 +1,24 @@
 const CartItem = {
-  init: (productService, cartService) => {
+  init: cartService => {
     const cartItemsElement = document.querySelector('#cart-items');
 
-    const { getProductById } = productService;
-    const { updateCartInfo, handleChangeQuantity, handleRemoveItem } = cartService;
+    const { updateCartItem, removeCartItem } = cartService;
 
     cartItemsElement.addEventListener('click', event => {
       const { target } = event;
 
       const productId = target.dataset.productId;
       const productElement = document.querySelector(`#${productId}`);
-      const product = getProductById(productId);
 
       if (target.classList.contains('quantity-change')) {
-        handleChangeQuantity(target, productElement, productId);
-      } else if (target.classList.contains('remove-item')) {
-        handleRemoveItem(productId, productElement);
+        const change = parseInt(target.dataset.change);
+        updateCartItem(change, productElement, productId);
+        return;
       }
 
-      updateCartInfo();
+      if (target.classList.contains('remove-item')) {
+        removeCartItem(productId, productElement);
+      }
     });
   },
   render: () => {},
